24e18ea77be54dc3092333efd47530e6da9c5670
JulianaBMattos <julianabarcellosmattos@gmail.com>
2022-03-25 17:24:17 +0000

    adjustments on page views

--- src/App.vue
+++ src/App.vue
@@ -37,6 +37,22 @@
       <div class="content has-text-centered">
         <p>Made by Catalyst Community for the Catalyst Community</p>
         <p><img class="aim-logo" src="@/assets/images/aim-logo.png" alt="Cardano AIM" /></p>
+        <p class="is-size-4 has-text-weight-bold">
+          <a href="https://cardanoscan.io/pool/b61f05ec1e907ab9b069eaec6c664056c16f56cab59076109c66d2ae" target="_blank">
+            Stake with [AIM] pool
+          </a>
+        </p>
+        <p class="icons">
+          <a href="https://github.com/Project-Catalyst/ca-tool" target="_blank">
+            <b-icon icon="github" size="small" />
+          </a>
+          <a href="https://twitter.com/AimCardano" target="_blank">
+            <b-icon icon="twitter" size="small" />
+          </a>
+          <a href="https://t.me/joinchat/Ivl50eWG7r0zODI1" target="_blank">
+            <b-icon icon="telegram" size="small" />
+          </a>
+        </p>
         <b-button
           label="Feedback"
           type="is-primary"
